6362bbd3daaee0d0809a49109e1a8ff4a8f14a20
Douglas Kihara <douglaskihara@gmail.com>
2025-11-18 18:59:18 -0300

    ajuste para script

--- sistemas-de-apoio-a-decisao/mvp-django-machine-learning/scripts/populate_treinos.py
+++ sistemas-de-apoio-a-decisao/mvp-django-machine-learning/scripts/populate_treinos.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import django
 
-# Configurar Django
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
